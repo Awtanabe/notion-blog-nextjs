ac6a9e24d695ca7d342e6270d89afa40960a17ea
akifumi_watanabe <akifumi_watanabe@akifumi-watanabenonotopasokon.local>
2023-03-18 11:44:26 +0900

    GA設置

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
 import "../styles/globals.css";
+import Script from "next/script";
+import * as gtag from '../lib/gatag';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  )
 }
 
 export default MyApp;
