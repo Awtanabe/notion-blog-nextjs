6ef500238e3410a5b0755676fd4bd2c96849361b
akifumi_watanabe <akifumi_watanabe@akifumi-watanabenoMacBook-Pro.local>
2023-07-05 22:11:58 +0900

    ga遷移

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import "../styles/globals.css";
 import Script from "next/script";
 import * as gtag from '../lib/gatag';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
   return(
     <>
       <Script
